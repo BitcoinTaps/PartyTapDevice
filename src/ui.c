2a71756c88633e4314a4002792e574334dc499a4
pieterjm <pieter@meulenhoff.org>
2024-02-17 21:18:24 +0100

    Corrected screen orientation

--- src/ui.c
+++ src/ui.c
@@ -1774,7 +1774,7 @@ void ui_init(void)
     lv_disp_t * dispp = lv_disp_get_default();
     lv_theme_t * theme = lv_theme_default_init(dispp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED),
                                                false, LV_FONT_DEFAULT);
-    //lv_disp_set_rotation(dispp, LV_DISP_ROT_0);
+    lv_disp_set_rotation(dispp, LV_DISP_ROT_180);
 
 
     lv_disp_set_theme(dispp, theme);
