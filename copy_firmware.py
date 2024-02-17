9cf582dfe8a1701ea776dafc4b7f993f1691dc7a
pieterjm <pieter@meulenhoff.org>
2024-02-17 20:26:51 +0100

    Added copy firmware script

---
+++ copy_firmware.py
@@ -0,0 +1,36 @@
+import shutil
+import configparser
+import re
+import json
+
+project_config = configparser.ConfigParser()
+project_config.read("platformio.ini")
+
+
+firmware_version = project_config.get("common", "firmware_version")
+pioenv = 'ESP32_3248S035C'
+build_dir = f".pio/build/{pioenv}"
+lnbits_dir = "lnbits"
+firmware_dir = f"{lnbits_dir}/static/firmware/{pioenv}"
+template_dir = f"{lnbits_dir}/templates/partytap"
+
+for filename in ['firmware.bin','partitions.bin','bootloader.bin']:
+    shutil.copyfile(f"{build_dir}/{filename}",f"{firmware_dir}/{filename}")
+
+# update manifest
+with open(f"{firmware_dir}/manifest.json","r") as file:
+        data = json.load(file)
+data['version'] = firmware_version
+with open(f"{firmware_dir}/manifest.json","w") as file:
+    json.dump(data,file,indent=4)
+
+# update version in index.html
+with open(f"{template_dir}/index.html","r") as file:
+    content = file.read()
+
+content = re.sub("version\: \'.*\',",f"version: \'{firmware_version}\',",content)
+
+with open(f"{template_dir}/index.html","w") as file:
+    file.write(content)
+
+    
